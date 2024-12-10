c850d34845a3e79cffe6072d28ecb606ba8016e1
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-09 19:56:11 -0500

    refactor(main): fix problem with deleting taskWrapperhtml element childs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
 const newTaskInput = document.querySelector(".input-task");
 const inputButton = document.querySelector(".input-button");
-const tasksWrapper = document.querySelector(".tasks-wrapper");
+let tasks = null;
+if(window.localStorage.getItem('tasks')){
+    tasks = JSON.parse(window.localStorage.getItem('tasks'));
+}else{
+    tasks = [];
+}
+function renderTasks(){
+    const tasksWrapper = document.querySelector(".tasks-wrapper");
+    tasksWrapper.innerHTML = "";
+    tasks.forEach(task => {
+        const taskDiv = document.createElement("div");
+        const taskSpan = document.createElement("span");
+        taskSpan.textContent = task.task;
+        taskDiv.appendChild(taskSpan);
+        tasksWrapper.appendChild(taskDiv);
+    });
+}
+renderTasks();
 inputButton.addEventListener("click",()=>{
-    const newTask = document.createElement("div");
-    const taskSpan = document.createElement("span");
-    taskSpan.textContent = newTaskInput.value;
-    newTaskInput.value = "";
-    newTask.appendChild(taskSpan);
-    tasksWrapper.appendChild(newTask);
+    console.log(newTaskInput.value);
+    console.log(newTaskInput.value.length);
+    if(newTaskInput.value.length > 0){
+
+        console.log("new task");
+        const newTask = {
+            task:newTaskInput.value,
+            complete:false,
+        };
+        tasks.push(newTask);
+        window.localStorage.setItem('tasks',JSON.stringify(tasks));
+        newTaskInput.value = "";
+        renderTasks();
+    }
 });
